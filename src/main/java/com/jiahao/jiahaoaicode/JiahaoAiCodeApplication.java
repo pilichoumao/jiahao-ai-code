887f4efe548ab71d87ec5612f13f43b88b5e6fae
pilichoumao <mao415488505@163.com>
2025-11-25 14:12:01 +0800

    jiahao-ai-code项目完成

--- src/main/java/com/jiahao/jiahaoaicode/JiahaoAiCodeApplication.java
+++ src/main/java/com/jiahao/jiahaoaicode/JiahaoAiCodeApplication.java
@@ -1,9 +1,15 @@
 package com.jiahao.jiahaoaicode;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
+
+@MapperScan("com.jiahao.jiahaoaicode.mapper")
+@EnableCaching
 public class JiahaoAiCodeApplication {
 
     public static void main(String[] args) {
